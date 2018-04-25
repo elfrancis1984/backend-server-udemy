302a28428c10d2648ae69396797074b1fa05b8cc
Francisco Chalán <francisco.chalan@enigmabuild.com>
2018-04-25 14:08:23 -0500

    Seccion 12 terminada . backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
 module.exports.SEED = '@este-@es-@un-@seed-@dificil';
+
+module.exports.CLIENT_ID = '520711999671-bqqicicaabuel22phdkgemaakuh221p6.apps.googleusercontent.com';
---
+++ config/signingoogle.js
@@ -0,0 +1,4 @@
+// ID de cliente 
+// 520711999671-bqqicicaabuel22phdkgemaakuh221p6.apps.googleusercontent.com
+// Secreto de cliente 
+// yEc2_YUPoj0EWTmtL_v4Y2_x
